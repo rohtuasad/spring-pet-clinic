ad5e891581abeb5531f6df4bb0763d397bc612f8
Alexey <rohtuasad@gmail.com>
2020-01-22 23:04:29 +0300

    Refactor services to common CRUD interface

--- pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package ru.rohtuasad.springpetclinic.services;
 
 import ru.rohtuasad.springpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findbyId(long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package ru.rohtuasad.springpetclinic.services;
 
 import ru.rohtuasad.springpetclinic.model.Vet;
 
-import java.time.LocalDate;
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
