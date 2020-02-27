306bc9d3661c4de34394370d58a586ebb0d21711
Alexey <rohtuasad@gmail.com>
2020-02-27 22:34:09 +0300

    Add list owners output on owners page

--- pet-clinic-web/src/main/java/ru/rohtuasad/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/rohtuasad/springpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package ru.rohtuasad.springpetclinic.controllers;
 
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.rohtuasad.springpetclinic.services.OwnerService;
 
 // Alternative way to map requests
 @RequestMapping("/owners")
 @Component
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
