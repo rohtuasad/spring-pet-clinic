df093eacc0f2637e226eeb2dcfebcf4378735818
Alexey Volkov <rohtuasad@gmail.com>
2020-04-13 00:29:49 +0300

    Add pet type map based service

---
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ru.rohtuasad.springpetclinic.services;
+
+import ru.rohtuasad.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package ru.rohtuasad.springpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import ru.rohtuasad.springpetclinic.model.PetType;
+import ru.rohtuasad.springpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
