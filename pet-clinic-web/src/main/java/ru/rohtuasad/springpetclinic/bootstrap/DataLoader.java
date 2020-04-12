0f6768967f7edeb8c7e0f47e373246425cf80bce
Alexey Volkov <rohtuasad@gmail.com>
2020-04-13 00:40:51 +0300

    Fill pet types on startup

--- pet-clinic-web/src/main/java/ru/rohtuasad/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ru/rohtuasad/springpetclinic/bootstrap/DataLoader.java
@@ -3,22 +3,34 @@ package ru.rohtuasad.springpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ru.rohtuasad.springpetclinic.model.Owner;
+import ru.rohtuasad.springpetclinic.model.PetType;
 import ru.rohtuasad.springpetclinic.model.Vet;
 import ru.rohtuasad.springpetclinic.services.OwnerService;
+import ru.rohtuasad.springpetclinic.services.PetTypeService;
 import ru.rohtuasad.springpetclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
