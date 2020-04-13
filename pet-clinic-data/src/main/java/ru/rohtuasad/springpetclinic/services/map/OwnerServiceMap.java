7d413c4b253424d9335e087af3220f342bf7e7c7
Alexey Volkov <rohtuasad@gmail.com>
2020-04-14 00:57:30 +0300

    Check if pet type exists before persist owner

--- pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package ru.rohtuasad.springpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import ru.rohtuasad.springpetclinic.model.Owner;
+import ru.rohtuasad.springpetclinic.model.Pet;
 import ru.rohtuasad.springpetclinic.services.OwnerService;
+import ru.rohtuasad.springpetclinic.services.PetService;
+import ru.rohtuasad.springpetclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,6 +36,23 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
+        if (owner == null) {
+            return null;
+        }
+        if (owner.getPets() != null) {
+            owner.getPets().forEach(pet -> {
+                if (pet.getPetType() != null) {
+                    pet.setPetType(petTypeService.save(pet.getPetType()));
+                } else {
+                    throw new RuntimeException("Pet type must be not null");
+                }
+
+                if (pet.getId() == null) {
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+        }
         return super.save(owner);
     }
 
