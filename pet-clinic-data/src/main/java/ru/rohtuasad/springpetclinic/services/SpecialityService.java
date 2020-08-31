e32b4040605cfcb73b812fa05d67eaca99b2191a
Alexey Volkov <rohtuasad@gmail.com>
2020-08-31 22:44:42 +0300

    Added speciality map service

---
+++ pet-clinic-data/src/main/java/ru/rohtuasad/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package ru.rohtuasad.springpetclinic.services;
+
+import ru.rohtuasad.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
